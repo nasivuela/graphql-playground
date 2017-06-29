53246ed8d47bff328cbcf22efd9b7aeb6766971a
Graphext <graphext@MacBook-Air-de-Graphext.local>
2017-06-29 16:36:24 +0200

    Add react-apollo and github auth

--- app/components/dashboard.js
+++ app/components/dashboard.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React from 'react'
+import { 
+  graphql,
+  gql, } from 'react-apollo'
 
 class Dashboard extends React.Component {
+
   render() {
+    console.log(this.props)
     return (
         <div>
           Hello World!
@@ -10,4 +15,9 @@ class Dashboard extends React.Component {
   } 
 }
 
-export default Dashboard
+
+export default graphql(gql`query{ 
+  repository(owner: "facebook", name: "react") {
+    id
+  }
+}`)(Dashboard)
--- index.js
+++ index.js
@@ -1,7 +1,34 @@
 import React from 'react';
 import { render } from 'react-dom';
 import Dashboard from './app/components/dashboard'
+import { 
+  ApolloClient,
+  ApolloProvider,
+  createNetworkInterface,
+} from 'react-apollo'
+import githubToken from './config'
+
+const networkInterface = createNetworkInterface({
+  uri: 'https://api.github.com/graphql',
+})
+
+networkInterface.use([{
+  applyMiddleware(req, next) {
+    if (!req.options.headers) {
+      req.options.headers = {};  // Create the header object if needed.
+    }
+    req.options.headers.authorization = `Bearer ${githubToken}`
+    next()
+  }
+}])
+
+const client = new ApolloClient({
+  networkInterface,
+})
+
 
 render((
-  <Dashboard />
+  <ApolloProvider client={client}>
+    <Dashboard />
+  </ApolloProvider>
 ), document.querySelector('#app'))
